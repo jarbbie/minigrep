81a268968ae41ee64494855464a60e168a43db3f
jb <peeranat.ngam@gmail.com>
2026-05-11 16:39:33 +0700

    feat: Complete minigrep implementation

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
+    // Read params: cargo run -- <query> <file_path>
     let args: Vec<String> = env::args().collect();
 
+    // Extract params
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
         });
 
     println!("Searching for \"{}\"", config.query);
     println!("In file \"{}\"", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    // Run minigrep
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -31,7 +37,29 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+        
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
